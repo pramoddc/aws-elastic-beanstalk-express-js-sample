57fa731eac5c2097e24dd35a19a0c4504e2e3a01
Pramod <pramod.d@redroad.in>
2020-12-30 15:35:37 +0530

    New commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Red Road Health Solutions.30/12/2020'));
+app.get('/', (req, res) => res.send('Hello Red Road Health Solutions.30/12/2020 new'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
